7e1814cf44b9545139d2c45085179bfd2b516b21
TanmayDesai <tdesai@gmail.com>
2022-11-07 23:11:19 +0530

    minor changes

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,35 @@
 let Tasks = [
     {
+        task: "Green card represents all the completed tasks",
+        isCompleted: true
+    },
+    {
+        task: "Red card represents all the pending tasks",
+        isCompleted: false
+    },
+    {
+        task: "Tapping on a card will change the status of the Task",
+        isCompleted: true
+    },
+    {
+        task: "Clear all tasks feature coming soon...",
+        isCompleted: false
+    },
+    {
         task: "Clean Table" ,
         isCompleted: true
     },
     {
         task: "Learn React",
         isCompleted: false
+    },
+    {
+        task: "Connect to database",
+        isCompleted: false
+    },
+    {
+        task: "Buy Groceries",
+        isCompleted: true
     },
 ];
 
